8ed7d43a2aacd04fd7aa828bd85e78455a9bb652
ivanuria <ivan.uria@gmail.com>
2024-09-17 17:03:23 +0200

    feat: Added FE and fixed cors

--- patientor-be/build/index.js
+++ patientor-be/build/index.js
@@ -4,15 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 const requestLogger = (req, _res, next) => {
     console.log(req.method, req.path);
     next();
 };
 app.use(requestLogger);
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- patientor-be/index.ts
+++ patientor-be/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import type Express from 'express';
+import cors from 'cors';
 
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 const requestLogger:Express.RequestHandler = (req, _res, next) => {
@@ -12,10 +15,10 @@ const requestLogger:Express.RequestHandler = (req, _res, next) => {
 app.use(requestLogger);
 
 // routes
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
